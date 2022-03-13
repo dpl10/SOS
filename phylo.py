f08e261b2e1e99b24dd8a500683dd34a597b01db
Nelson Salinas <nrsalinas@gmail.com>
2022-03-12 23:12:40 -0500

    Encoding draft. Stills replicates characters through inverted encoding.

--- phylo.py
+++ phylo.py
@@ -107,6 +107,14 @@ class Treell:
 			self.adj_table[i,d] = 1
 			self.adj_table[d,i] = 1
 
+		# Result table. Index meaning:
+		# First dimension = main node
+		# Second dimension = excluded node
+		#
+		# Values
+		# 0 = Not computed yet
+		# 1 = Negative
+		# 2 = Positive
 		self.results = np.zeros_like(self.adj_table)
 
 		return None
@@ -116,29 +124,35 @@ class Treell:
 		children = np.where(th == 1)[0]
 		children = children[children != excluded]
 		leaves, no_leaves = [], []
+		
 		for x in children:
 			leaves.append(x) if x in self.taxa else no_leaves.append(x)
+		
 		for child in no_leaves:
-			leaves += self.leaves_from_node(child)
+			leaves += self.leaves_from_node(child, node)
+		
 		return leaves
 
-	def orthology_test_(self, target_node: int, excluded_node: int) -> bool:
+
+	def orthology_test(self, target_node: int, excluded_node: int) -> bool:
+		"""
+		Test orthology condition on a node of an unrooted tree. The test excludes 
+		one set of descendants of the node, to be set by `excluded_node` argument.
+		Returns boolean. Updates table at `self.result`. 
+		"""
 		
 		pass_test = True
 		
 		if self.results[target_node, excluded_node] == 0:
-
+			
 			r = self.adj_table[target_node]
 			icr = np.where(r == 1)[0]
 			icr = icr[icr != excluded_node]
-			names = []
 			name_origin = {}
-
+			
 			for child in icr:
-
+				a_son_failed = False
 				if child in self.taxa:
-					names.append(self.taxa[child])
-
 					if not self.taxa[child] in name_origin:
 						name_origin[self.taxa[child]] = 1
 					else:
@@ -148,35 +162,42 @@ class Treell:
 					thtest = self.orthology_test(child, target_node)
 
 					if thtest:
-						thnames = self.leaves_from_node(child, target_node)
-						names += thnames
-
+						thleaves = self.leaves_from_node(child, target_node)
+						thnames = [self.taxa[x] for x in thleaves]
+						
 						for tn in thnames:
 							if tn in name_origin:
 								name_origin[tn] += 1
-
 							else:
 								name_origin[tn] = 1
+
 					else:
-						pass_test = False
+						a_son_failed = True
 						break
 
-			#print(f"{names=}")
-			#print(f"{name_origin=}")
+			#print(f"{name_origin=}\n")
 
-			if len(name_origin) == 1:
-				pass_test = True
-
-			elif len(name_origin) > 1:
-
-				for tn in name_origin:
-
-					if name_origin[tn] > 1:
-						pass_test = False
-						break
+			if a_son_failed:
+				pass_test = False
 
 			else:
-				pass_test = False
+				if len(name_origin) == 1:
+					pass_test = True
+
+				elif len(name_origin) > 1:
+					for tn in name_origin:
+						if name_origin[tn] > 1:
+							pass_test = False
+							break
+
+				else: # redundant?
+					pass_test = False
+
+			if pass_test:
+				self.results[target_node, excluded_node] = 2
+			else:
+				self.results[target_node, excluded_node] = 1
+
 
 		elif self.results[target_node, excluded_node] == 1:
 			pass_test = False
@@ -186,89 +207,96 @@ class Treell:
 
 		return pass_test
 
-	def orthology_test(self, target_node: int, excluded_node: int) -> list:
-		"""
-		Test orthology condition on a node of an unrooted tree. The test excludes 
-		one set of descendants of the node, to be set by `excluded_node` argument.
-		Returns an empty list if fails, or a list of the terminal taxa if passes. 
-		"""
 
-		pass_test = True
-		r = self.adj_table[target_node]
-		icr = np.where(r == 1)[0]
-		icr = icr[icr != excluded_node]
-		names = []
-		name_origin = {}
-
-		for child in icr:
-
-			if child in self.taxa:
-				names.append(self.taxa[child])
-
-				if not self.taxa[child] in name_origin:
-					name_origin[self.taxa[child]] = 1
-				else:
-					name_origin[self.taxa[child]] += 1
-
-			else:
-				thnames = self.orthology_test(child, target_node)
-
-				if len(thnames) == 0:
-					pass_test = False
-					name_origin = {}
-					break
-
-				else:
-					names += thnames
-
-					for tn in thnames:
-						if tn in name_origin:
-							name_origin[tn] += 1
-
-						else:
-							name_origin[tn] = 1
-
-		#print(f"{names=}")
-		#print(f"{name_origin=}")
-
-		if len(name_origin) == 1:
-			pass_test = True
-
-		elif len(name_origin) > 1:
-
-			for tn in name_origin:
-
-				if name_origin[tn] > 1:
-					pass_test = False
-					break
-
-		else:
-			pass_test = False
-
-		if pass_test:
-			names = list(set(names))
-			return names
-
-		else:
-			return []
-
-
-	def ortholog_finder(self):
-		internal_ = np.zeros_like(self.adj_table)
-		leaves = [x for x in range(self.adj_table.shape[0]) if x in self.taxa]
-		internal_[leaves] = 0
-		internal_[:,leaves] = 0
-		int_coors = np.where(internal_ > 0)
-
-		print(internal_)
-
+	def ortholog_encoder(self):
+		encoded_edges = []
+		encoding = []
+		labels = [x for x in self.labels]
+		internal = np.copy(self.adj_table)
+		leaves = [x for x in self.taxa]
+		internal[leaves] = 0
+		internal[:,leaves] = 0
+		int_coors = np.where(internal > 0)
 		for i,d in zip(int_coors[0], int_coors[1]):
-			testi = self.orthology_test(i,d)
-			testd = self.orthology_test(i,d)
+			_ = self.orthology_test(i,d)
+			_ = self.orthology_test(d,i)
+		
+		# Get starting nodes for traversal
+		inits = {}
+		for i,d in combinations(leaves, 2):
+			pa0 = self.adj_table[i]
+			pa1 = self.adj_table[d]
+			pa0 = np.where(pa0 == 1)[0][0]
+			pa1 = np.where(pa1 == 1)[0][0]
 
-			if len(testi):
-				#encode descendants excluding
-				pass
+			if pa0 == pa1:
+				if not pa0 in inits:
+					inits[pa0] = {i:0, d:0}
+				else:
+					inits[pa0][i] = 0
+					inits[pa0][d] = 0
+
+		# Find orthologous clades
+		print(inits)
+		for start in inits:
+			prev_node = None
+			curr_node = start
+			curr_excluded = list(inits[start].keys())
+			still = True
+			
+			while still:
+				neighs = self.get_neighbors(curr_node, curr_excluded)
+				curr_excluded = [curr_node] + labels
+				cands = []
+			
+				for nei in neighs:
+					test = self.results[curr_node, nei]
+			
+					if test == 2:
+						cands.append(nei)
+					else:
+						curr_excluded.append(nei)
+			
+				if len(cands) == 0:
+					still = False
+			
+				elif len(cands) == 1:
+					prev_node = curr_node
+					curr_excluded.append(curr_node)
+					curr_node = cands[0]
+			
+				else:
+					choosen = None
+					choosen_size = 0
+			
+					for c in cands:
+						th = len(self.leaves_from_node(curr_node, c))
+						if choosen_size < th:
+							choosen = c
+							choosen_size = th
+			
+					prev_node = curr_node
+					curr_excluded += [c for c in cands if c != choosen]
+					curr_node = choosen
+
+			# encode char
+			if (curr_node, prev_node) in encoded_edges:
+				continue
+			else:
+				encoded_edges.append((curr_node, prev_node))
+				encoding.append( [0 for x in self.labels] ) 
+				thleaves = self.leaves_from_node(curr_node, prev_node)
+				for l in thleaves:
+					encoding[-1][labels.index(l)] = 1
+		
+		return encoding
+
+
+	def get_neighbors(self, node, excluded=[]):
+		th = self.adj_table[node]
+		th = np.where(th > 0)[0]
+		th = th[~np.isin(th, excluded)]
+		return th
 
 
 if __name__ == "__main__":
@@ -288,14 +316,15 @@ if __name__ == "__main__":
 	for le in al.lengths:
 		print(le, al.lengths[le])
 
-	print(al.adj_table)
+	print(al.adj_table, "\n")
 
-	print(al.orthology_test(0, 15))
+	#print(al.leaves_from_node(4,5))
+	#print(al.leaves_from_node(5,4))
 
-	al.ortholog_finder()
-
-
-				
-
-
+	print(al.orthology_test(17, 15))
+	print(al.orthology_test(15, 17))
 	
+	#print(al.labels.keys())
+	#print(al.ortholog_encoder())
+
+			
